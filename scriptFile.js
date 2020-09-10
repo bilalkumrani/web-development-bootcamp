355b9e90a86a79f4c7d73bc95966578447cdd417
Bilal Khan <B.umrani75@gmail.com>
2020-09-10 22:29:13 +0500

    some files

---
+++ scriptFile.js
@@ -0,0 +1,21 @@
+document.write("Hello world <br>");
+document.write("Bilal here<br>");
+//alert box
+alert("ye to very nice alert h");
+
+//confirm box
+var a = confirm("kia ap insan hen?");
+if (a)
+{
+	document.write("welcome insan");
+}
+else
+{
+	document.write("welcome Janwar");
+}
+
+
+//pomt box
+var b = prompt("Dinner me cha khdo huyai?");
+document.write("<br>" +b);
+document.write("<br> Wokey?");
