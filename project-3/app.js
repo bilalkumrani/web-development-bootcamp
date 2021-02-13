5649c9fd7ebedbfcd560397fb13672dffc75a49a
Bilal Khan <b.umrani75@gmail.com>
2021-02-13 19:47:21 +0500

    ending mongo

---
+++ project-3/app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 80;
+
+//Express stuff
+app.use('/static',express.static('static'));
+app.use(express.urlencoded());
+
+//pug stuff
+
+app.set('view engine', 'pug'); //set the template engine as pug
+app.set('views', path.join(__dirname,'views')); // set the views directory
+
+
+//endpoints
+app.get('/',(req,res)=>{
+    res.status(200).render('home.pug');
+});
+app.get('/contact',(req,res)=>{
+    res.status(200).render('contact.pug');
+});
+
+//start server
+app.listen(port,()=>{
+    console.log("Server started on "+port);
+});
+
+
